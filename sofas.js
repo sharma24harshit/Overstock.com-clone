03edbf0a9f1201bbbc01aa4ad3e4d17b589ebf0e
SubodhSingh8543 <subodhsingh8543@gmail.com>
2022-11-10 22:25:10 +0530

    day-3done

---
+++ sofas.js
@@ -0,0 +1,242 @@
+
+let data = [
+      {
+        image: "https://ak1.ostkcdn.com/images/products/is/images/direct/c37288160e04adcb11c3a4eb5b87f3949a2f835a/Serta-Palisades-Upholstered-61%22-Loveseat-Modern-Design%2C-Straight-Arms.jpg?imwidth=480&impolicy=medium",
+        name1: "Serta Palisades Upholstered 61 Loveseat Modern Design, Straight Arms",
+        price: 41177,
+      },
+      {
+          image: "https://ak1.ostkcdn.com/images/products/is/images/direct/8ca26cdda8e84d66bdeb5cd266e99bd55291eea8/Square-Arms-Upholstered-Fabric-Sofa-with-Pillows.jpg?imwidth=480&impolicy=medium",
+          name1: "Morden Loveseat Square Arms Upholstered Fabric Sofa with Pillows",
+          price: 31830,
+      },
+      {
+          image: "https://ak1.ostkcdn.com/images/products/is/images/direct/02efa8e68f2e6f1bff70efb3fcae0e31dbde967c/Velvet-3-seater-Reversible-Modular-Sofa.jpg?imwidth=480&impolicy=medium",
+          name1: "Velvet 3-seater Reversible Modular Sofa",
+          price: 158355,
+      },
+      {
+          image: "https://ak1.ostkcdn.com/images/products/is/images/direct/a5a9c6dc5837206e62052c93326ed77233a9cd3d/Queen-size-Futon-with-Butternut-Finish-and-Suede-Mattress.jpg",
+          name1: "Butternut Finish Hardwood Queen Futon with Suede Mattress",
+          price: 5676600,
+      },
+      {
+          image: "https://ak1.ostkcdn.com/images/products/is/images/direct/f9b82771ddc4bced07bb0c03f209c2ed7d414eed/Zahra-7-piece-Fabric-Sectional-Sofa-Set-by-Christopher-Knight-Home.jpg",
+          name1: "Zahra Fabric 7-piece Sectional Sofa Set by Christopher Knight Home",
+          price: 75017,
+      },
+      {
+          image: "https://ak1.ostkcdn.com/images/products/is/images/direct/d382ccf7b16a554c17b0e187cbe52c2f6d80ed6e/Linen-Solid-Wood-Legs-Loveseat.jpg",
+          name1: "Linen Solid Wood Legs Loveseat",
+          price: 27557,
+      },
+      {
+                image: "https://ak1.ostkcdn.com/images/products/is/images/direct/696a26f965a1db3bf2c6b1db928a2931f0ad78d4/Rosevera-Bjorn-Chesterfield-Settee.jpg?imwidth=480&impolicy=medium",
+                name1: "Rosevera Bjorn Chesterfield Settee",
+                price: 28987,
+             },
+              {
+                  image: "https://ak1.ostkcdn.com/images/products/is/images/direct/ea61ead910df39ecea615a1254f15fab001fcad7/Two-Person-Iron-Frame-Back-Wooden-Frame-Seat-Loveseat.jpg?imwidth=480&impolicy=medium",
+                  name1: "Two-Person Iron Frame Back Wooden Frame Seat Loveseat",
+                  price: 20259,
+              },
+              {
+                  image: "https://ak1.ostkcdn.com/images/products/is/images/direct/c62452d27577b5cf25ef6f45bdbc067ceaeb61b9/Classic-3-seat-Sofa-with-Nails-for-Living-Room.jpg?imwidth=480&impolicy=medium",
+                  name1: "Classic 3-seat Sofa with Nails for Living Room",
+                  price: 47528,
+              },
+              {
+                  image: "https://ak1.ostkcdn.com/images/products/is/images/direct/7eeab2551a6da96ccb1fa752b5adc1fac224f974/Morden-Fort-Modern-Contemporary-Sofa-Couch-with-Deep-Button-Tufting-Dutch-Velvet%2C-Solid-Wood-Frame-and-Iron-Legs.jpg?imwidth=480&impolicy=medium",
+                  name1: "Morden Fort Modern Button Tufted Dutch Velvet Sofa",
+                  price: 81412,
+              },
+              {
+                  image: "https://ak1.ostkcdn.com/images/products/is/images/direct/593580270110bd74b6bcd88ee7d7bcdcabd72a4b/Fabric-Living-Room-Sofa-Couch-Selctional-Sofa-L-Shape-Chaise-Left-and-Right-Interchangeable-Linen-Corner-Sofa-with-Ottoman.jpg?imwidth=480&impolicy=medium",
+                  name1: "Fabric Living Room Sofa Couch Selctional Sofa L Shape Chaise Left and Right Interchangeable Linen Corner Sofa with Ottoman",
+                  price: 85682,
+              },
+              {
+                      image: "https://ak1.ostkcdn.com/images/products/10184613/Jaxx-Zipline-Queen-size-Convertible-Sleeper-Loveseat-and-Ottomans-Bed-8a51de72-6fd6-44ef-a536-8a24b970055d_600.jpg?imwidth=480&impolicy=medium",
+                      name1: "Jaxx Zipline Convertible Queen-size Sleeper Loveseat and Ottoman Set",
+                      price: 67741,
+                  },
+                  {
+                    image: "https://ak1.ostkcdn.com/images/products/is/images/direct/4fa3cc577280ea707f19b0f567b941ef6aaa48d8/Dark-blue-linen-sofa%2C-70%22-loveseat%2C-Two-seater-sofa.jpg?imwidth=480&impolicy=medium",
+                    name1: "Dark blue linen sofa, 70 loveseat, Two-seater sofa, Tufted Couch - 70 x 30x 31",
+                    price: 59981,
+                  },
+                  {
+                      image: "https://ak1.ostkcdn.com/images/products/is/images/direct/7c12327875bc7a178d64e61edbc4156b6c2b395c/HOMCOM-Convertible-Floor-Sofa-Bed%2C-Recliner-Armchair-Upholstered-Sleeper-Chair-with-Pillow-for-Living-Room-Bedroom-Lounge%2C-Grey.jpg?imwidth=480&impolicy=medium",
+                      name1: "HOMCOM Convertible Floor Sofa Bed, Recliner Armchair Upholstered Sleeper Chair with Pillow for Living Room Bedroom Lounge, Grey",
+                      price: 17810,
+                  },
+                  {
+                      image: "https://ak1.ostkcdn.com/images/products/is/images/direct/5fefa8fdc8f4b5e3e189538c6d53e0a04ec69288/Barto-Comfy-45%22Loveseat-with-Tufted-Back.jpg?imwidth=480&impolicy=medium",
+                      name1: "Barto Comfy 45 Loveseat with Tufted Back",
+                      price: 31300.14,
+                  },
+                  {
+                      image: "https://ak1.ostkcdn.com/images/products/is/images/direct/7cbca178980e5a6c1435ef8ab82cb875048dadcd/Modern-2-Seater-Manual-Recline-Sofabed-Velvet-Tufted-Back%26Seat-Accent-Loveseat-with-2-Pillows-and-Rose-Gold-Metal-Legs.jpg",
+                      name1: "Modern 2 Seater Manual Recline Sofabed Velvet Tufted Back&Seat Accent Loveseat with 2 Pillows and Rose Gold Metal Legs",
+                      price: 45560,
+                  },
+                  {
+                      image: "https://ak1.ostkcdn.com/images/products/is/images/direct/93c956f82e6987cddc8d23888e2bd09c2ddeade6/Morden-Fort-Luxury-Classic-America-Chesterfield-Tufted-Camel-Back-Armchair-Living-Room-Sofa.jpg?imwidth=480&impolicy=medium",
+                      name1: "Morden Fort Luxury Classic America Chesterfield Tufted Camel Back Sofa",
+                      price: 82669.47,
+                  },         
+                  {
+                    image: "https://ak1.ostkcdn.com/images/products/is/images/direct/df7052fd301933d77d31fb368b66c8ae34918b73/Velvet-Sofa-%2C-Accent-sofa-.loveseat-sofa-with-iron-feet.jpg?imwidth=480&impolicy=medium",
+                    name1: "Velvet Sofa , Accent sofa .loveseat sofa with iron feet",
+                    price: 33054.27,
+                },
+                {
+                    image: "https://ak1.ostkcdn.com/images/products/is/images/direct/2f9a842d46771baec040d9314d99f76aa33d6f8c/Linen-Solid-Wood-Legs-Single-Seat---Concubine-Solid-Wood-Frame-Can-Be-Combined-With-Two-Seat-and-Three-Seats-Indoor-Modular-Sofa.jpg?imwidth=480&impolicy=medium",
+                    name1: "Linen Solid Wood Legs Single Seat - Concubine Solid Wood Frame Can Be Combined With Two-Seat and Three Seats Indoor Modular Sofa",
+                    price: 31541.33,
+                 },
+                  {
+                      image: "https://ak1.ostkcdn.com/images/products/is/images/direct/fe1ddefa38c1f83af15e36665f8960b93a74b276/Modern-Minimalist-Comfortable-Casual-Standard-Tufted-Back-Sofa-with-Pocket-Spring-and-Manufactured-Wood-for-Home-Living-Room.jpg",
+                      name1: "Modern Minimalist Comfortable Casual Standard Tufted Back Sofa with Pocket Spring and Manufactured Wood for Home Living Room",
+                      price: 79427,
+                  },
+                  {
+                      image: "https://ak1.ostkcdn.com/images/products/is/images/direct/f92bf98a9b70fb5133ebf8005813aff89a418499/Velvet-Padded-Seat-Convertible-Loveseat-Sleeper-Sofa-Adjustable-Backrest-Couch.jpg?imwidth=480&impolicy=medium",
+                      name1: "Velvet Padded Seat Convertible Loveseat Sleeper Sofa Adjustable Backrest Couch",
+                      price: 62489.74,
+                  },
+                  {
+                      image: "https://ak1.ostkcdn.com/images/products/is/images/direct/3486c74c0af6b3a18124ae63a8a8cf1cd5b62e2a/Velvet-Sofa-Bed-with-Cup-Holders-Sleeper-Couch-Sofa-with-2-Pillows.jpg?imwidth=480&impolicy=medium",
+                      name1: "Velvet Sofa Bed with Cup Holders Sleeper Couch Sofa with 2 Pillows - 69” Sofa Bed",
+                      price: 55258.55,
+                  },
+                  {
+                      image: "https://ak1.ostkcdn.com/images/products/is/images/direct/77b478a50fd1c12e894aeed659ac216aea27e286/Convertible-Sofa-Bed-Square-Arm-Tufted-Velvet-Sofa-Loveseat.jpg?imwidth=480&impolicy=medium",
+                      name1: "Convertible Sofa Bed Square Arm Tufted Velvet Sofa Loveseat",
+                      price: 37542.81,
+                  },
+                  {
+                      image: "https://ak1.ostkcdn.com/images/products/is/images/direct/456eefc6f57dc82f2fc295518f0752f816876bfe/Morden-Fort-Couches-for-Living-Room%2C-Sofas-for-Living-Room-Furniture-Sets%2C-Sofa%2C-Fabric%2C-Velvet.jpg?imwidth=480&impolicy=medium",
+                      name1: "Morden Fort Tufted Velvet Upholstered Chesterfield Sofa",
+                      price: 53723.69,
+                  },
+                  {
+                      image: "https://ak1.ostkcdn.com/images/products/is/images/direct/0dd70ccfc76f1ed3f8be7f87aa1bfd0ffe310efb/Modern-Linen-3-Seat-Sofa-with-Flared-Arms%2C-Cushion-Back-%26-Seat-and-Plastic-Block-Legs.jpg?imwidth=480&impolicy=medium",
+                      name1: "Modern Linen 3 Seat Sofa with Flared Arms, Cushion Back & Seat and Plastic Block Legs",
+                      price: 44400.34,
+                  },
+                  {
+                      image: "https://ak1.ostkcdn.com/images/products/is/images/direct/ca961dc7b0e90f208c9d536ebb0205e32cc7078e/Futzca-63%27%27-Small-Modern-Loveseat-Couch%2C-2-Seat-Couch-Tufted-Love-Seat-for-Small-Space.jpg?imwidth=480&impolicy=medium",
+                      name1: "Futzca 63-inch Small Modern Tufted Upholstered Loveseat",
+                      price: 37974.95,
+                  },
+                  {
+                      image: "https://ak1.ostkcdn.com/images/products/is/images/direct/0f8a5e4ad7072710efb4b0ec8bb0c6919e3253bc/Sectional-Sofa-with-Movable-Ottoman.jpg?imwidth=480&impolicy=medium",
+                      name1: "Sectional Sofa with Movable Ottoman",
+                      price: 66395.40,
+                  },
+                  {
+                      image: "https://ak1.ostkcdn.com/images/products/is/images/direct/fab3df548720d836df6a211a9f6b4211b9fd72e7/Morden-Fort-Modern-Sofa-with-Deep-Dutch-Velvet%2C-Iron-Legs.jpg?imwidth=480&impolicy=medium",
+                      name1: "Morden Fort Deep Dutch Velvet Modern Sofa",
+                      price: 78812.22,
+      },
+      {
+        image: "https://ak1.ostkcdn.com/images/products/is/images/direct/a7efa630d19bb42d1838f5714cfddb38e320e421/Kotter-Home-Curved-Back-Boucle-Loveseat.jpg?imwidth=480&impolicy=medium",
+        name1: "Kotter Home Curved Back Boucle Loveseat",
+        price: 38790.80,
+    },
+    {
+        image: "https://ak1.ostkcdn.com/images/products/is/images/direct/2fc154b87a1aad1bf2aa6cfb36d9658827691fc7/75%22-Fabric-Upholstered-L-Shaped-3-Seat-Sofa-with-Reversible-Ottoman%2C-Comefortable-Cushion-and-Solid-Wood-Legs-for-Living-Room.jpg?imwidth=480&impolicy=medium",
+        name1: "75 Fabric Upholstered L-Shaped 3 Seat Sofa with Reversible Ottoman, Comefortable Cushion and Solid Wood Legs for Living Room",
+        price: 56040.59,
+    },
+    {
+        image: "https://ak1.ostkcdn.com/images/products/is/images/direct/7b3126c7d4720adc8bad398b8b461d748f270104/Green-Twin-Size-with-Trundle-and-Three-Drawers-Upholstered-Daybed.jpg?imwidth=480&impolicy=medium",
+        name1: "Green Twin Size with Trundle and Three Drawers Upholstered Daybed",
+        price: 69661.54,
+    },
+    {
+        image: "https://ak1.ostkcdn.com/images/products/is/images/direct/835162b6f35eaf9ee05e27ef5f00a72cde29e0cc/Purple-Velvet-Accent-Chair-with-Ottoman%2C-Modern-Arm-Chair-And-Footstool-Set-with-Golden-Legs-for-Living-Room-Bedroom.jpg?imwidth=480&impolicy=medium",
+        name1: "Purple Velvet Accent Chair with Ottoman, Modern Arm Chair And Footstool Set with Golden Legs for Living Room Bedroom",
+        price: 64524.34,
+    },
+    {
+        image: "https://ak1.ostkcdn.com/images/products/is/images/direct/d65cf08f368c40b490a9b521e6621ede08c86aa2/Modern-Style-Polyfiber-Adjustable-Chaise-Bed-Living-Room-Solid-wood-Legs-Tufted-Couch.jpg",
+        name1: "Modern Style Polyfiber Adjustable Chaise Bed Living Room Solid wood Legs Tufted Couch",
+        price: 51594.99,
+    },
+    {
+        image: "https://ak1.ostkcdn.com/images/products/is/images/direct/8a9dcb6827253b61ef81b2b18a335f6ded88ec24/HOMCOM-Swivel-Floor-Chair-with-Back-Support%2C-Microfiber-Adjustable-Video-Gaming-Chair%2C-Grey.jpg?imwidth=480&impolicy=medium",
+        name1: "HOMCOM Swivel Floor Chair with Back Support, Microfiber Adjustable Video Gaming Chair, Grey",
+        price: 12001.13,
+    },
+    {
+        image: "https://ak1.ostkcdn.com/images/products/is/images/direct/c908bee4e1028b5e75ad9085009c30f1feadc716/L-Shape-Sofa-Polyester-Fabric-Padded-Free-Combination-Sofa-Living-Room-Furniture-with-Gold-Metal-Legs-%26-%28three-Seat%2BOttoman%29.jpg",
+        name1: "L Shape Sofa Polyester Fabric Padded Free Combination Sofa Living Room Furniture with Gold Metal Legs & (three Seat+Ottoman)",
+        price: 86992.65,
+    },
+    {
+        image: "https://ak1.ostkcdn.com/images/products/is/images/direct/82c40afa13e5b1ac49f4eb434f71f83b0dc11bc1/Sleeper-Sofa-Velvet-Pull-Out-Bed-Adjustable-Backrest-2-Pillows-%26-Side-Pocket.jpg?imwidth=480&impolicy=medium",
+        name1: "Velvet Sofa Pull-Out Sleeper Bed Adjustable Backrest with 2 Pillows",
+        price: 72935.91,
+    },
+    {
+        image: "https://ak1.ostkcdn.com/images/products/is/images/direct/c24ef903c3abd608905a3c5f7e6d70991a0a7fef/Cream-Sofa-Couch-Velvet-Tufted-Upholstered-with-Pillows-and-Metal-Legs.jpg?imwidth=480&impolicy=medium",
+        name1: "Cream Sofa Couch Velvet Tufted Upholstered with Pillows and Metal Legs",
+        price: 75806.46,
+    },
+    {
+        image: "https://ak1.ostkcdn.com/images/products/is/images/direct/5547d6708e2c11d652387f656c3c1a12cca59c55/Upholstered-Fabric-Sofa-with-Pillows-and-Square-Arms.jpg?imwidth=480&impolicy=medium",
+        name1: "Morden Velvet Loveseat Sofa Couch with Pillows for Living Room",
+        price: 33461.73,
+    },
+    {
+        image: "https://ak1.ostkcdn.com/images/products/is/images/direct/de6b21b6d91efe67e4ccc66eefd872715bb2c854/HOMCOM-2-In-1-Design-Convertible-Footrest-Stool-Single-Sofa-Bed-with-Side-Pocket-and-Metal-Frame-for-Living-Room-Bedroom.jpg?imwidth=480&impolicy=medium",
+        name1: "HOMCOM 2-In-1 Design Convertible Footrest Stool Sofa Bed - 33.75*28.25*17.75",
+        price: 26146.49,
+    },
+    ]
+
+
+  function appendItems(data){
+    document.getElementById("div2Second").innerHTML = null;
+    data.forEach(function(el,i){
+       console.log(el.name1,el.price)
+       let div1 = document.createElement('div');
+       div1.addEventListener("click",function(){
+           localStorage.setItem('viewItem',JSON.stringify(el));
+       });
+
+       let img = document.createElement("img");
+       img.src = el.image;
+       img.classList.add("divImg");
+
+       let titlename1 = document.createElement('p');
+       titlename1.innerText = el.name1
+
+       let price = document.createElement("h5");
+       price.innerText ="Sale Start at INR "+ el.price;
+
+    //    div1.append(img,title,price);
+       div1.append(img,price,titlename1);
+
+       document.querySelector("#div2Second").append(div1);
+
+    });
+  }
+
+  appendItems(data);
+
+document.querySelector("#selectTag").addEventListener("change",function(){
+    let selected = document.querySelector("#selectTag").value;
+        if(selected == "HightoLow"){
+          //desending
+          data.sort((a,b) => b.price - a.price);
+        } 
+        if(selected == "LowtoHigh"){
+          //asending
+          data.sort((a,b) => a.price - b.price)
+        }
+        appendItems(data);
+});
+
